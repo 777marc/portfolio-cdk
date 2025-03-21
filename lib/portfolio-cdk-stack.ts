04606336041d2b3dd131f5440406be1f00a7b548
Marc Mendoza <777marc@gmail.com>
2025-03-21 12:06:18 -0400

    init

--- lib/portfolio-cdk-stack.ts
+++ lib/portfolio-cdk-stack.ts
@@ -1,16 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+
+interface PortfolioSiteStackProps extends cdk.StackProps {
+  domainName: string;
+  siteSubDomain: string;
+}
 
 export class PortfolioCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: PortfolioSiteStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const {domainName, siteSubDomain} = props;
+    const siteDomain = `${siteSubDomain}.${domainName}`;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PortfolioCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const siteBucket = new s3.Bucket(this, 'SiteBucket', {
+      bucketName: siteDomain,
+      websiteIndexDocument: 'indexedDB.html',
+      websiteErrorDocument: 'error.html',
+      publicReadAccess: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
   }
 }
