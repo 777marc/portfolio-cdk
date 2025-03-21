ccc7be60ac3110bff253bea559bb03838f3c95eb
Marc Mendoza <777marc@gmail.com>
2025-03-21 15:36:42 -0400

    add infra logic and content

--- bin/portfolio-cdk.ts
+++ bin/portfolio-cdk.ts
@@ -3,4 +3,6 @@ import * as cdk from 'aws-cdk-lib';
 import { PortfolioCdkStack } from '../lib/portfolio-cdk-stack';
 
 const app = new cdk.App();
-new PortfolioCdkStack(app, 'PortfolioCdkStack', {});
+new PortfolioCdkStack(app, 'PortfolioCdkStack', {
+    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'us-east-1' },
+});
